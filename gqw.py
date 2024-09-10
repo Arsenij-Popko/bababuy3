40eeb3b55f5c7c9cd4895bad7b6eaa52691f02c2
Arsenij-Popko <arsenijpopko2008@gmail.com>
2024-09-10 08:42:37 +0300

    Add files via upload

---
+++ gqw.py
@@ -0,0 +1,18 @@
+# Створення змінних
+greeting = "Hello World"
+name = "Арсеній"
+surname = "Попко"
+age = 16
+
+# Виведення типу та значення змінних
+print("Тип змінної greeting", type(greeting))
+print(greeting)
+
+print("Тип змінної name", type(name))
+print(name)
+
+print("Тип змінної surname", type(surname))
+print(surname)
+
+print("Тип змінної age", type(age))
+print(age)
